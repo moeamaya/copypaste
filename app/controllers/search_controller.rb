c0f2dcfc2819af19c2d26350e775df1e5214dc7f
moeamaya <moeamaya@gmail.com>
2013-06-29 22:28:02 -0700

    ajax

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,7 +12,7 @@ class SearchController < ApplicationController
    
     respond_to do |format|
       format.html  { render :text => @web_result }
-      format.js { render :text => @web_result }
+      format.json { render :text => @web_result }
     end
   end
   
