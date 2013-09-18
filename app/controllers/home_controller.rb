ee048ba1c23e022e58d91da4985137de220bfbc4
moeamaya <moeamaya@gmail.com>
2013-09-17 23:27:36 -0400

    postgres sort

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   # GET /links
   def index
     #@links = Links.order('created_at DESC').order('position DESC').all
-    @links = Links.all(:order => 'DATE(created_at) DESC, DATE(position) DESC')
+    @links = Links.all(:order => 'DATE(position) DESC')
 
 
     respond_to do |format|
