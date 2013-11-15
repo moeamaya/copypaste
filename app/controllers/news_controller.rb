a643403027865bdfdf54bf3d7b2c665ab6752d25
moeamaya <moeamaya@gmail.com>
2013-11-14 19:30:58 -0500

    added working twitter trends

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,18 @@
 class NewsController < ApplicationController
   def index
-    iron_worker = IronWorkerNG::Client.new
-    iron_worker.tasks.create("hello", "foo"=>"bar")
+    client = initTwitter()
+
+    @trends = client.trends(2459115)
+  end
+
+  private
+
+  def initTwitter
+    client = Twitter.configure do  |config|
+      config.consumer_key        = "5vthI8St0cgtsEBsVViq0Q"
+      config.consumer_secret     = "SWwpoF9ploz3HVW07IfhhEViU0VNJthlyTyhPI5Ms"
+      #config.access_token        = "240337585-jLmFywQJGGlxdlt8Jvdj4GVhuIzxiCxFUkPGTt3b"
+      #config.access_token_secret = "fCRGCYJ1gl7jRcBdxis4gLBwyzgWNTOeIIYlGpJ0JlfvP"
+    end
   end
 end
